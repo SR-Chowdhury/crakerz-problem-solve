49b95b6a6ec4b0dfcd31e088be1249f99dd9789d
Shihan Chowdhury <hackedgdown@gmail.com>
2023-02-06 10:40:58 +0000

    find-average done

--- count-number-zero.js
+++ count-number-zero.js
@@ -1,11 +1,17 @@
 /** 
- * Count the number of all zero : [X]
+ * Count the number of all zero : [done]
 */ 
 
 function countZeros(binary_num) {
-    var x = binary_num.includes('0');
-    // console.log(binary_num);
-    return x;
+    // var x = binary_num.includes('0');
+    let sum = 0;
+    for (let i = 0; i < binary_num.length; i ++) {
+        // console.log(binary_num[i]);
+        if (binary_num[i] == '0') {
+            sum++;
+        }
+    }
+    return sum;
 }
 
 var result = countZeros('10101');
--- find-average.js
+++ find-average.js
@@ -1,19 +1,17 @@
 /**
- * Find the average : [X]
+ * Find the average : [done]
  */
 function average(arrOfMarks) {
 
-    var sum = 0, avg;
+    let sum = 0, avg, roundUp;
     for (let i = 0; i< arrOfMarks.length; i++) {
-        sum+=arrOfMarks[i];
+        sum += arrOfMarks[i];
     }
+
     avg = sum / arrOfMarks.length;
-    if(Number.isInteger(avg)) {
-        return avg;
-    }
-    else {
-        return avg.toFixed(2);
-    }
+    roundUp = avg.toFixed(2);
+    // console.log(typeof parseFloat(roundUp));
+    return parseFloat(roundUp);
 }
 
 var arrOfMarks = [100, 100, 100, 100, 100];
